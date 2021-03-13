a5f9ae7a2d2b7e65bd06818278b3cd3fa0d63758
grant-nebbs <gnebbergall@gmail.com>
2021-03-12 21:48:00 -0500

    finished all methods

--- methods.rb
+++ methods.rb
@@ -12,6 +12,7 @@ class Driver
   end
 end
 
+drivers = Array.new
 
 def timeToHours(time)
   splitTime = time.split(':', 1)
@@ -19,6 +20,14 @@ def timeToHours(time)
   return hours.to_i
 end
 
+def findDriver(name)
+  for driver in drivers
+    if driver.name === name
+      return driver
+    end
+  end
+  print("Name not found in drivers")
+end
 
 def getAvgSpeed(startT, endT, distance)
   numOfHours = timeToHours(endT) - timeToHours(startT)
@@ -29,10 +38,21 @@ def processLine(line)
   lineArray = line.split
   command = lineArray[0]
   if(command === "Driver")
-    return Driver.new(lineArray{1]
+    drivers.push(Driver.new(lineArray[1]))
   else if(command === "Trip")
-    
+    driver = findDriver(lineArray[1])
+    driver.trip(getAvgSpeed(lineArray[2], lineArray[3], lineArray[4]), lineArray[4])
   else 
     print("Incorrect formatting of line")
+  end
+end
+
+def printOutput
+  for driver in drivers
+    if driver.avgSpeed < 100 && driver.avgSpeed > 5
+      puts(driver.name + ": " + driver.distance + " miles @ " + driver.avgSpeed + " mph")
+    else
+      puts(driver.name + ": 0 miles"
+    end
   end
 end
